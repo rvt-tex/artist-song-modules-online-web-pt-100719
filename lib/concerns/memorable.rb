4a5c48e7498133dff9a6621719dd37d5fcb3ed07
Ruel Tingling <rvtingling@gmail.com>
2019-11-08 17:29:17 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,16 +1,18 @@
 
-
 module Memorable
-  
   module ClassMethods
- 
-  def self.reset_all
-    self.all.clear
+    def reset_all
+      self.all.clear
+    end
+
+    def count
+      self.all.count
+    end
   end
 
-  def self.count
-    self.all.count
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
   end
-  
 end
-end 
