92991e09b07b8845130883fe7fd9529730fd35c8
Ruel Tingling <rvtingling@gmail.com>
2019-11-08 17:30:45 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -10,9 +10,9 @@ module Memorable
     end
   end
 
- # module InstanceMethods
-  #  def initialize
-  #    self.class.all << self
-  #  end
- # end
+ module InstanceMethods
+   def initialize
+     self.class.all << self
+   end
+   end
 end
